c6367a8097825e76c411ae064df1f24b019918e6
Troy Kinsella <troy.kinsella@gmail.com>
2015-09-01 21:52:44 -0700

    Added immutable view feature.

---
+++ test/immutable-view-test.js
@@ -0,0 +1,37 @@
+var assert = require('assert');
+var fs = require('fs');
+var LinkedMap = require('../lib/linked-map');
+
+describe('immutable-view', function() {
+
+  it('should have a predictable initialized state', function () {
+    var lm = new LinkedMap();
+    var v = lm.immutableView();
+    assert.strictEqual(0, v.size());
+    assert.strictEqual(null, v.headKey());
+    assert.strictEqual(null, v.tailKey());
+    assert.strictEqual(null, v.head());
+    assert.strictEqual(null, v.tail());
+    assert.deepEqual([], v.keys());
+    assert.deepEqual([], v.values());
+  });
+
+  it('should error on mutating operations', function() {
+    var lm = new LinkedMap();
+    var v = lm.immutableView();
+
+    [
+      'unshift',
+      'push',
+      'shift',
+      'pop',
+      'remove',
+      'clear'
+    ].forEach(function(m) {
+        assert.throws(function() {
+          v[m]();
+        });
+      });
+  });
+
+});
